2ebcdca6f93dfa3bed6304581b03def407a2051d
caregnuz <caregnatocristian865@gmail.com>
2025-11-30 21:28:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 
 global.botnumber = '';
 global.confirmCode = '';
-global.nomebot = '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲-𝐁𝐨𝐭';
+global.nomebot = '𝖇ᵢˢc𝘰𝕥𝕥𝘰🥀🧸';
 global.packname = '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲-𝐁𝐨𝐭';
 global.author = '𝐌𝐝';
 global.vs = '8.4';
@@ -15,12 +15,12 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['393773842461', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
-  ['xxxxxxxxxx'], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx']
+  ['393792120860', 'cristian💋', true],
+  [''], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
+  [''],
+  [''],
+  [''],
+  ['']
 ];
 global.mods = ['xxxxxxxxxx'];
 global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx'];
